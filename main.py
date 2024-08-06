8baae46d92a9864c8dafc5585c6230ba40132572
ejmcd0 <164799844+ejmcd0@users.noreply.github.com>
2024-08-06 17:48:19 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,138 @@
+import random
+
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean
+
+'''
+Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+
+# CREATE DB
+class Base(DeclarativeBase):
+    pass
+# Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# Cafe TABLE Configuration
+class Cafe(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    map_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    location: Mapped[str] = mapped_column(String(250), nullable=False)
+    seats: Mapped[str] = mapped_column(String(250), nullable=False)
+    has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
+
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# HTTP GET - Read Record
+@app.route("/random", methods=['GET'])
+def random_cafe():
+    all_cafes = db.session.execute(db.select(Cafe)).scalars().all()
+    random_cafe = random.choice(all_cafes)
+    return jsonify(random_cafe.to_dict())
+    # a longer way to do the above return, if we hadnt turned it into a dictionary first
+    # return jsonify(id = random_cafe.id,
+    #                name = random_cafe.name,
+    #                map_url = random_cafe.map_url,
+    #                img_url = random_cafe.img_url,
+    #                location = random_cafe.location,
+    #                seats = random_cafe.seats,
+    #                has_toilet = random_cafe.has_toilet,
+    #                has_wifi = random_cafe.has_wifi,
+    #                has_sockets = random_cafe.has_sockets,
+    #                can_take_calls = random_cafe.can_take_calls,
+    #                coffee_price = random_cafe.coffee_price)
+
+@app.route("/all")
+def all_shops():
+    all_cafes = db.session.execute(db.select(Cafe).order_by(Cafe.id)).scalars().all()
+    cafe_dict = [cafe.to_dict() for cafe in all_cafes] #turn each row into a dict
+    return jsonify(cafe_dict)
+
+@app.route("/search")
+def search():
+    cafe_loc = request.args.get("loc")
+    result = db.session.execute(db.select(Cafe).where(Cafe.location == cafe_loc)).scalars().all()
+    return jsonify(cafes=[row.to_dict() for row in result])
+
+
+### USING POSTMAN.COM TO GET/POST/PATCH/DELETE
+
+# HTTP POST - Create Record
+@app.route('/add', methods=['POST'])
+def add_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    success = {'response': {'success': 'Successfully added cafe'}}
+    return success
+# HTTP PUT/PATCH - Update Record
+@app.route("/update-price/<int:cafe_id>", methods=['PATCH'])
+def update_price(cafe_id):
+    new_price = request.args.get('new_price')
+    cafe = db.get_or_404(Cafe, cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify({"success": "Successfully updated price"})
+    else:
+        return jsonify(error={"error": "ID does not exist"})
+
+
+
+# HTTP DELETE - Delete Record
+@app.route('/report-closed/<cafe_id>', methods=['DELETE'])
+def delete_cafe(cafe_id):
+    delete_cafe = db.get_or_404(Cafe, cafe_id)
+    db.session.delete(delete_cafe)
+    db.session.commit()
+    if cafe_id:
+        return jsonify(response={"success": "Successfully deleted"})
+    else:
+        return jsonify(error={"Error": "ID not found"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
